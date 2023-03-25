9c066d7815f45d4bba3fbe4953432b0f35881962
karol.rutkowski <karol.rutkowski@bowwe.com>
2023-03-25 15:22:28 +0000

    feat: add cors

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router(require('./src/mods/db.js')())
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const cors = require('cors');
+const server = jsonServer.create();
+const router = jsonServer.router(require('./src/mods/db.js')());
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
+const corsOptions = {
+    origin:'http://localhost:63342',
+    credentials:true,
+    optionSuccessStatus:200
+};
+
+server.use(cors(corsOptions));
 const port = 4500;
 server.listen(port, function () {
     console.log(`JSON Server is running on port ${port}`)
-})
+});
